2d6436ae661672f4a2d7d9089b1f828a20ed8581
Charles Knight <Charles-Knight@users.noreply.github.com>
2020-06-26 19:51:13 -0700

    moved output to terminal from 'main' to notify() and added OS check before creating notification

--- rHWS.py
+++ rHWS.py
@@ -11,7 +11,7 @@ reddit API, not because its a better way to do this, but because it will provide
 me with a greater learning experience.
 '''
 
-import praw
+import praw, sys, os
 
 '''
 Get new posts from targeted subreddit
@@ -57,24 +57,34 @@ def print_posts(posts):
 '''
 Notify user
 Simple notification method. Currently just creates an apple script notifcation
-to notify on macOS.
-TODO: extend with other functionality - email? system notifications? sms?
+to notify on macOS and outputs to terminal.
+TODO: extend with other functionality - email? system notifications? sms? Need
+      to offer more info: title, author, time, link, (stretch prices parsed from
+      post body or title)
 '''
-def notify(title, message):
-    os.system("""
-              osascript -e 'display notification "{}" with title "{}"'
-              """.format(text, title))
+def notify(post_title, message, words):
+
+    print(post_title)
+    print("Matches: ",end="")
+    print(', '.join(words))
+
+    if sys.platform.startswith('darwin'):
+        os.system("""
+                  osascript -e 'display notification "{}" with title "New post matching {}"'
+                  """.format(post_title, ", ".join(words)))
 
 if __name__ == "__main__":
     sub = "hardwareswap"
-    keywords = ["RTX","DDR4"]
+    keywords = ["RTX","DDR4","IPS"]
 
     reddit = praw.Reddit('bot1')
     subreddit = reddit.subreddit(sub)
 
     # Create stream and view each new post
     for submission in subreddit.stream.submissions():
-        print(submission.title)
-    # Check for keywords and if found save details somewhere ad notify
-    # want to note: post title, time, author, and link
-    # Also TODO: put find_keywords outside of match_posts
+
+        # Check for keywords and if found save details somewhere and notify
+        # want to note: post title, time, author, and link
+        matched_words = find_keywords(submission.title, keywords)
+        if len(matched_words) > 0:
+            notify(submission.title, "", matched_words)
